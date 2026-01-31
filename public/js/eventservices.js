b060273db5eda54e65ecb00fe3cd214a4f3d9d30
DodgeRich14 <troydass1614@gmail.com>
2026-01-31 12:50:09 +0800

    1 to 1 store and job listings css from initial eden + minor overall changes

--- public/js/eventservices.js
+++ public/js/eventservices.js
@@ -29,33 +29,33 @@ document.addEventListener('DOMContentLoaded', function() {
       const image = document.getElementById('previewImage');
       
       if (selectedValue === 'image1') {
-        image.src = './Assets/Images/Gender_Reveal_Event.jpg';
+        image.src = './uploads/Gender_Reveal_Event.jpg';
       } else if (selectedValue === 'image2') {
-        image.src = './Assets/Images/Baptismal_Event.jpg'; // Replace with actual path
+        image.src = './uploads/Baptismal_Event.jpg'; // Replace with actual path
       } else if (selectedValue === 'image3') {
-        image.src = './Assets/Images/Debut_Event.webp'; // Replace with actual path
+        image.src = './uploads/Debut_Event.webp'; // Replace with actual path
       } else if (selectedValue === 'image4') {
-        image.src = './Assets/Images/Graduation_Ball.jpg'; // Replace with actual path
+        image.src = './uploads/Graduation_Ball.jpg'; // Replace with actual path
       } else if (selectedValue === 'image5') {
-        image.src = './Assets/Images/Outdoor_Wedding.jpg'; // Replace with actual path
+        image.src = './uploads/Outdoor_Wedding.jpg'; // Replace with actual path
       } else if (selectedValue === 'image6') {
-        image.src = './Assets/Images/Beach_Wedding.jpg'; // Replace with actual path
+        image.src = './uploads/Beach_Wedding.jpg'; // Replace with actual path
       } else if (selectedValue === 'image7') {
-        image.src = './Assets/Images/Anniversary_Event.jpg'; // Replace with actual path
+        image.src = './uploads/Anniversary_Event.jpg'; // Replace with actual path
       } else if (selectedValue === 'image8') {
-        image.src = './Assets/Images/Funeral_Event.jpeg'; // Replace with actual path
+        image.src = './uploads/Funeral_Event.jpeg'; // Replace with actual path
       } else if (selectedValue === 'image9') {
-        image.src = './Assets/Images/Birthday_Party.jpg'; // Replace with actual path
+        image.src = './uploads/Birthday_Party.jpg'; // Replace with actual path
       } else if (selectedValue === 'image10') {
-        image.src = './Assets/Images/Welcome_Party.webp'; // Replace with actual path
+        image.src = './uploads/Welcome_Party.webp'; // Replace with actual path
       } else if (selectedValue === 'image11') {
-        image.src = './Assets/Images/Mothers_Day.jpg'; // Replace with actual path
+        image.src = './uploads/Mothers_Day.jpg'; // Replace with actual path
       } else if (selectedValue === 'image12') {
-        image.src = './Assets/Images/Fathers_Day.png'; // Replace with actual path
+        image.src = './uploads/Fathers_Day.png'; // Replace with actual path
       } else if (selectedValue === 'default') {
-        image.src = './Assets/Images/event_reservation_preview.png'; // Replace with actual path
+        image.src = './uploads/event_reservation_preview.png'; // Replace with actual path
       } else {
-        image.src = './Assets/Images/event_reservation_preview.png'; // Default image
+        image.src = './uploads/event_reservation_preview.png'; // Default image
       }
     }
   });
